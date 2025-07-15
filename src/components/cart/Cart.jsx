38896d08f3cb3f3a95a5a3db338a023a5e2bacd8
riseagain66 <riseagain66@gmail.com>
2025-07-15 17:40:49 +0500

    #3 Работа с корзиной - добавление/удаление товаров.

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,7 +5,7 @@ import {CartPriceRow} from "../cart-price-row/CartPriceRow.jsx";
 import {Icon} from "../icon/Icon.jsx";
 
 export function Cart({orderedProducts, changeOrderedProducts}) {
-    const totalPrice = orderedProducts.length>1 ? orderedProducts.reduce((acc, item) => acc + item.totalPrice, 0) : orderedProducts[0]?.totalPrice || 0;
+    const totalPrice = orderedProducts.length > 1 ? orderedProducts.reduce((acc, item) => acc + item.totalPrice, 0) : orderedProducts[0]?.totalPrice || 0;
     const deliveryValue = totalPrice !== 0 ? 16 : 0;
     const totalOrderPrice = (totalPrice + deliveryValue).toFixed(2);
 
@@ -14,38 +14,42 @@ export function Cart({orderedProducts, changeOrderedProducts}) {
             <div className="cart">
                 <div className="cart__order-wrapper">
                     <div className="cart__product-list">
-                        {orderedProducts?.map((item) => (<CartProduct key={item?.id} order={item} changeOrderedProducts={changeOrderedProducts}/>))}
+                        {orderedProducts?.map((item) => (
+                            <CartProduct key={item?.id} order={item} changeOrderedProducts={changeOrderedProducts}/>))}
                     </div>
-                    <div className="cart__order">
-                        <div className="cart__title">You Order</div>
-                        <div className="cart__order-price-wrapper">
-                            <CartPriceRow type="price" rowName="Order price" value={totalPrice}  info=""/>
-                            <CartPriceRow type="promocode" rowName="Discount for promo code" value="No" info=""/>
-                            {deliveryValue !== 0 && <CartPriceRow type="delivery" rowName="Delivery" value={deliveryValue} info="(Aug 02 at 16:00)"/>}
-                            <CartPriceRow type="total" rowName="Total" value={totalOrderPrice} info=""/>
+                    <div className="cart__promo-code-wrapper">
+                        <div className="cart__info">
+                            <div className="cart__title">You Have A Promo Code?</div>
+                            <div className="cart__description">To receive up-to-date promotional codes, subscribe to us
+                                on
+                                social networks.
+                            </div>
                         </div>
-                        <div className="button-wrapper cart__button-wrapper">
-                            <button className="button cart__button">Checkout</button>
-                            <div className="vertical-line cart__vertical-line"></div>
+                        <div className="cart__promo-code">
+                            <input type="text" name="promo-code" className="input" placeholder="Enter promo code"/>
+                            <div className="button-wrapper cart__promo-code-button-wrapper">
+                                <button className="button cart__promo-code-button">
+                                    <Icon name="arrow" className="icon_medium"/>
+                                </button>
+                                <div className="vertical-line cart__vertical-line"></div>
+                            </div>
                         </div>
+                        <FindUs/>
                     </div>
                 </div>
-                <div className="cart__promo-code-wrapper">
-                    <div className="cart__info">
-                        <div className="cart__title">You Have A Promo Code?</div>
-                        <div className="cart__description">To receive up-to-date promotional codes, subscribe to us on social networks.
-                        </div>
+                <div className="cart__order">
+                    <div className="cart__title">Your Order</div>
+                    <div className="cart__order-price-wrapper">
+                        <CartPriceRow type="price" rowName="Order price" value={totalPrice} info=""/>
+                        <CartPriceRow type="promocode" rowName="Discount for promo code" value="No" info=""/>
+                        {deliveryValue !== 0 && <CartPriceRow type="delivery" rowName="Delivery" value={deliveryValue}
+                                                              info="(Aug 02 at 16:00)"/>}
+                        <CartPriceRow type="total" rowName="Total" value={totalOrderPrice} info=""/>
                     </div>
-                    <div className="cart__promo-code">
-                        <input type="text" name="promo-code" className="input" placeholder="Enter promo code"/>
-                        <div className="button-wrapper cart__promo-code-button-wrapper">
-                            <button className="button cart__promo-code-button">
-                                <Icon name="arrow" className="icon_medium"/>
-                            </button>
-                            <div className="vertical-line cart__vertical-line"></div>
-                        </div>
+                    <div className="button-wrapper cart__button-wrapper">
+                        <button className="button cart__button">Checkout</button>
+                        <div className="vertical-line cart__vertical-line"></div>
                     </div>
-                    <FindUs/>
                 </div>
             </div>
         </div>
