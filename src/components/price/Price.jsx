a380d67e92f900621a8aac8c28638c8a4e676d58
riseagain66 <riseagain66@gmail.com>
2025-07-26 00:42:31 +0500

    #8 фикс компонента фильтрации по цене

--- src/components/price/Price.jsx
+++ src/components/price/Price.jsx
@@ -1,8 +1,10 @@
+import './Price.scss';
+
 export function Price({minPrice, maxPrice, changeMinPrice, changeMaxPrice}) {
     return (
         <div>
             <div className="h4">Price</div>
-            <div className="sidebar__price-bar">
+            <div className="price-bar">
                 <input type="text" placeholder={minPrice} className="input price"
                        onChange={(e) => changeMinPrice(e)}/>
                 <input type="text" placeholder={maxPrice} className="input price"
