6edfb8cf06ff5ff001d6abfc13d279fb12ccb96d
riseagain66 <riseagain66@gmail.com>
2025-07-27 18:58:42 +0500

    #11 сортировка товаров

--- src/components/showcase/Showcase.jsx
+++ src/components/showcase/Showcase.jsx
@@ -15,27 +15,19 @@ export function Showcase() {
     const [selectedCategory, setSelectedCategory] = useState("All");
     const [prices, setPrices] = useState({min: 0, max: Math.round(filters().prices.max) || 0})
     const [selectedColors, setSelectedColors] = useState([]);
+    const [selectedSort, setSelectedSort] = useState('');
     const productCount = productsToView?.length;
 
-    //применение фильтров и поиск с учетом фильтрации
+    //применение фильтров и поиск с учетом фильтрации и сортировки
     useEffect(() => {
         const filtersApplied = {
             category: selectedCategory,
             prices: prices,
             colors: selectedColors,
         }
-        const filteredProducts = filterProducts([...data.products], filtersApplied);
+        const filteredProducts = sortProducts(filterProducts([...data.products], filtersApplied), selectedSort);
         setProductsToView(debouncedSearchTerm.length > 0 ? filteredProducts.filter(product => product.name.includes(debouncedSearchTerm)) : filteredProducts);
-    }, [searchTerm, debouncedSearchTerm, selectedCategory, prices, selectedColors]);
-
-
-    function changeSelectedCategory(category) {
-        setSelectedCategory(category);
-    }
-
-    function changeSelectedColors(colors) {
-        setSelectedColors(colors);
-    }
+    }, [selectedSort, searchTerm, debouncedSearchTerm, selectedCategory, prices, selectedColors]);
 
     return (
         <div className="container">
@@ -50,24 +42,29 @@ export function Showcase() {
                     changeSelectedColors={changeSelectedColors}
                 />
                 <div className="showcase__products-wrapper">
+
                     <div className="showcase__sort-and-count">
                         <div>
                             There are <span className="showcase__bold">{productCount}</span> products in this item
                         </div>
                         <div className="showcase__sort">
-                            <select className="input showcase__input">
+                            <select className="input showcase__input" onChange={(e) => setSelectedSort(e.target.value)}>
                                 <option value="RELEVANCE">By relevance</option>
-                                <option value="ASC">ASC</option>
-                                <option value="DESC">DESC</option>
+                                <option value="NAME_AZ">From A to Z</option>
+                                <option value="NAME_ZA">From Z to A</option>
+                                <option value="PRICE_ASC">From low to high</option>
+                                <option value="PRICE_DESC">From high to low</option>
                             </select>
                         </div>
                     </div>
+
                     <div className="showcase__products">
-                        {(productsToView.length > 0)
+                    {(productsToView.length > 0)
                             ? productsToView.map(product => <Product key={product?.id} product={product}/>)
                             : <>No products found</>
                         }
                     </div>
+
                     <div className="showcase__pagination">
                         <div>
                             <Icon name="left-pagin-arrow" className="icon_pagin-arrow"/>
@@ -85,5 +82,33 @@ export function Showcase() {
             </div>
         </div>
     )
+
+    function changeSelectedCategory(category) {
+        setSelectedCategory(category);
+    }
+
+    function changeSelectedColors(colors) {
+        setSelectedColors(colors);
+    }
+
+    function sortProducts (products, sortType) {
+        switch (sortType) {
+            case "RELEVANCE":
+                break;
+            case "NAME_AZ":
+                products.sort((a, b) => a.name > b.name ? 1 : -1);
+                break;
+            case "NAME_ZA":
+                products.sort((a, b) => a.name < b.name ? 1 : -1);
+                break;
+            case "PRICE_ASC":
+                products.sort((a, b) => a.price > b.price ? 1 : -1);
+                break;
+            case "PRICE_DESC":
+                products.sort((a, b) => a.price < b.price ? 1 : -1);
+                break;
+        }
+        return products;
+    }
 }
 
