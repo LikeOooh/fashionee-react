29e759f5f5e32fa3e20f7194109015160c118a99
riseagain66 <riseagain66@gmail.com>
2025-08-01 13:03:55 +0500

    GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,22 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/fashionee-react/',
     plugins: [
         react(),
-        VitePluginSvgSpritemap('./src/icons/*.svg',
-            {
-                svgo: false,
+        VitePluginSvgSpritemap('./src/icons/*.svg', {
+            svgo: false,
 
-                // Дополнительные настройки вывода
-                output: {
-                    svg: {
-                        sizes: false // не добавлять свои размеры
-                    },
-                    svg4everybody: false // отключаем полифилл
+            // Дополнительные настройки вывода
+            output: {
+                svg: {
+                    sizes: false, // не добавлять свои размеры
                 },
-
-            }
-        ),
+                svg4everybody: false, // отключаем полифилл
+            },
+        }),
     ],
-})
+});
